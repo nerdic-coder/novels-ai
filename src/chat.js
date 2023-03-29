c76d63cdeab076cebf28c624865bc0d70680f4a4
TheNerdicCoder <johan@efdapps.com>
2023-03-29 21:53:15 +0300

    more dynamic backend

--- src/chat.js
+++ src/chat.js
@@ -1,15 +1,15 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  organization: `${process.env.chatOrganization}`,
-  apiKey: `${process.env.chatApiKey}`,
+  organization: process.env.chatOrganization,
+  apiKey: process.env.chatApiKey,
 });
 
 const openai = new OpenAIApi(configuration);
 
 export default async function createChatResponse(content) {
   return openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: process.env.chatModel || 'gpt-3.5-turbo',
     user: 'johan',
     messages: [{
       name: 'johan',
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import functions from '@google-cloud/functions-framework';
 import { v4 } from 'uuid';
+import Handlebars from 'handlebars';
 import createChatResponse from './chat.js';
 import generateSpeech from './speech.js';
 
@@ -7,21 +8,35 @@ functions.http('generate', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   const requestId = v4();
   const chapters = req.query.chapters || req.body.chapters || 3;
-  const starring = req.body.starring ? `Starring ${req.body.starring}.` : '';
+  const starring = req.query.starring || req.body.starring ? `Starring ${req.query.starring || req.body.starring}.` : '';
 
-  let story = `Write an audiobook, a ${req.query.genre || req.body.genre || 'book'} 
-    in the style of ${req.query.style || req.body.style || 'book'}.
-    ${starring}
-    Start with Chapter 1, only 2 paragraphs (a total of ${chapters} chapters), don't write out "Chapter N"`;
+  const story = process.env.chatPrompt || `Write a short story with the title "{{title}}", the genre is "{{genre}}".
+    in the style of "{{style}}".
+    {{starring}}
+    Start with Chapter 1, only 2 paragraphs (a total of {{chapters}} chapters), use present tense, don't write out "Chapter N", add some character dialogs.`;
+
+  // Compile the template
+  const template = Handlebars.compile(story);
+
+  // Fill in the placeholders with values from req.query, req.body, and other variables
+  const context = {
+    genre: req.query.genre || req.body.genre || 'book',
+    style: req.query.style || req.body.style || 'general',
+    starring,
+    chapters,
+    title: req.query.title || req.body.title || '',
+  };
+
+  let filledInStory = template(context);
 
   for (let chapter = 1; chapter <= chapters; chapter += 1) {
     if (chapter === chapters) {
-      story += ', the ending';
+      filledInStory += ', the ending';
     }
     // eslint-disable-next-line no-await-in-loop
-    const completion = await createChatResponse(story);
+    const completion = await createChatResponse(filledInStory);
     generateSpeech(completion.data.choices[0].message.content, `${requestId}/chapter-${chapter}`);
-    story += `\n${completion.data.choices[0].message.content}\nContinue with Chapter ${chapter + 1}, only 2 paragraphs`;
+    filledInStory += `\n${completion.data.choices[0].message.content}\nContinue with Chapter ${chapter + 1}, only 2 paragraphs`;
   }
 
   res.send(requestId);
