e492991a8be973de1ea5d6d70b010df9e6e2eecf
Johan Axelsson <johan@nerdic-coder.com>
2024-11-14 22:53:38 +0200

    sentry logging

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,5 +1,5 @@
-import { ApplicationConfig } from '@angular/core';
-import { InMemoryScrollingFeature, InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
+import { APP_INITIALIZER, ApplicationConfig, ErrorHandler } from '@angular/core';
+import { InMemoryScrollingFeature, InMemoryScrollingOptions, provideRouter, Router, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -9,6 +9,9 @@ import { DOCUMENT } from '@angular/common';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+
+import * as Sentry from "@sentry/angular";
+
 import { environment } from '../environments/environment.loader';
 
 const scrollConfig: InMemoryScrollingOptions = {
@@ -21,6 +24,20 @@ const inMemoryScrollingFeature: InMemoryScrollingFeature =
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler(),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
     provideRouter(routes, inMemoryScrollingFeature),
     provideClientHydration(),
     {
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import * as Sentry from "@sentry/angular";
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+Sentry.init({
+  dsn: "https://9d54a6a41af0eb42757c5c5386762ec1@o209365.ingest.us.sentry.io/4508298243145728",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", "novels-ai.com"],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
