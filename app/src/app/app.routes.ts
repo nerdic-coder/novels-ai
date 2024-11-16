9a84db6889f8fe43faa89939f1c9fa1535c9e761
Johan Axelsson <johan@nerdic-coder.com>
2024-11-16 15:16:30 +0200

    Exclude /__/auth/** from Angular routing

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -52,5 +52,7 @@ export const routes: Routes = [
   {
     path: '**',
     loadComponent: () => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent)
-  }
+  },
+  // Exclude /__/auth/** from Angular routing
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
