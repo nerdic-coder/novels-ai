aea7c178ce4079d7d8c93562f9a6920c39f625e2
Johan Axelsson <johan@nerdic-coder.com>
2025-01-24 17:27:31 +0200

    Publish the Audiobook to iTunes/Apple #27

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   API_URL_ADD_CHAPTER: 'https://add-chapter-i3rsqvn75a-lm.a.run.app/',
   API_URL_GENERATE_NEW_NOVEL: 'https://generate-audiobook-i3rsqvn75a-lm.a.run.app/',
   API_URL_REMOVE_NOVEL: 'https://delete-audiobook-i3rsqvn75a-lm.a.run.app/',
-  API_URL_EXPORT_AUDIOBOOK: 'https://europe-west2-ai-audiobook.cloudfunctions.net/export',
+  API_URL_EXPORT_AUDIOBOOK: 'https://export-i3rsqvn75a-nw.a.run.app',
   
   STORIES_PER_PAGE: 5,
   NEW_USER_POINTS: 1,
